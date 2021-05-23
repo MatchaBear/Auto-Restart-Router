49868c4a24f49e857491adb2f6e7edfd18a2547f
berry <bernhard.hustomo@gmail.com>
2021-05-23 23:43:20 +0800

    JADI .. !!

--- automation.py
+++ automation.py
@@ -2,12 +2,27 @@ from selenium import webdriver
 # from .edge.webdriver import WebDriver as ChromiumEdge
 import time
 
+time.sleep(3)
 web = webdriver.Chrome()
 web.get('http://192.168.0.253/webpages/login.html')
 
 time.sleep(2)
 
-LastName = "Berry"
-last = web.find_element_by_xpath('//*[@id="form-login"]/div[2]/div/div/div[1]/span[2]/input[1]')
-last.send_keys(LastName)
+PassForm = "admin"
+# last = web.find_element((By.XPATH,"//*[@id="form-login"]/div[2]/div/div/div[1]/span[2]/input[1]"))
+last = web.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/form[3]/div[2]/div/div/div[1]/span[2]/input[1]')
 
+last.send_keys(PassForm)
+
+Submit = web.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/form[3]/div[3]/div/div/div/div[1]/button')
+Submit.click()
+
+time.sleep(4)
+
+Submit2 = web.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/a[4]/span[1]')
+Submit2.click()
+
+time.sleep(2)
+
+Submit3 = web.find_element_by_xpath('/html/body/div[5]/div[2]/div[4]/div/div/div[2]/div/div[2]/button')
+Submit3.click()
---
+++ staticip-autofill+restart.bat
@@ -0,0 +1,30 @@
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+netsh interface ip set address name = "Ethernet" static 192.168.0.252 255.255.255.0
+
+REM --> start /MIN /B chrome --new-window --incognito "http://192.168.0.253/webpages/login.html"
+
+python3 automation.py
