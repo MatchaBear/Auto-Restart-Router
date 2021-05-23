aa3975af71501f3f0b290e6ea9dc19b8fbf215c5
berry <bernhard.hustomo@gmail.com>
2021-05-23 16:11:56 +0800

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
+# from .edge.webdriver import WebDriver as ChromiumEdge
 import time
 
 web = webdriver.Chrome()
-web.get('#url)
+web.get('http://192.168.0.253/webpages/login.html')
 
 time.sleep(2)
 
 LastName = "Berry"
-last = web.find_element_by_xpath('#element)
+last = web.find_element_by_xpath('//*[@id="form-login"]/div[2]/div/div/div[1]/span[2]/input[1]')
 last.send_keys(LastName)
 
