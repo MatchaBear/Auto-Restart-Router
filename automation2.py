bb60523ce0539d216d0c0b72d9a5a7e121e1372f
berry <50803250+MatchaBear@users.noreply.github.com>
2022-03-17 22:14:58 +0800

    eureka
    with find_element new syntax

--- automation2.py
+++ automation2.py
@@ -1,26 +1,28 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 time.sleep(3)
 # driver_path = 'C/Windows'
 # web = webdriver.Chrome(executable_path=driver_path)
-web = webdriver.Chrome()
-web.get('http://192.168.0.253/webpages/login.html')
+webdr = webdriver.Chrome()
+webdr.get('http://192.168.0.253/webpages/login.html')
 
 # wait to load the page
 time.sleep(2)
 # wait to load the page
 
 PassForm = "admin"
-last = web.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/form[3]/div[2]/div/div/div[1]/span[2]/input[1]')
+# last = webdr.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/form[3]/div[2]/div/div/div[1]/span[2]/input[1]')
+last = webdr.find_element(By.XPATH, '//*[@id="form-login"]/div[2]/div/div/div[1]/span[2]/input[1]')
 
 # enter admin password
 last.send_keys(PassForm)
 # enter admin password
 
 # click Login button
-Submit = web.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/form[3]/div[3]/div/div/div/div[1]/button')
+Submit = webdr.find_element(By.XPATH,'//*[@id="login-btn"]/span[2]')
 Submit.click()
 # click Login button
 
@@ -29,7 +31,7 @@ time.sleep(4)
 # wait to load page after logging in
 
 # click Reboot button
-Submit2 = web.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[2]/a[4]/span[1]')
+Submit2 = webdr.find_element(By.XPATH,'//*[@id="txt-reboot"]')
 Submit2.click()
 # click Reboot button
 
@@ -38,6 +40,6 @@ time.sleep(2)
 # wait for the popup to show
 
 # click Yes at the popup to reboot
-Submit3 = web.find_element_by_xpath('/html/body/div[5]/div[2]/div[4]/div/div/div[2]/div/div[2]/button')
+Submit3 = webdr.find_element(By.XPATH,'//*[@id="reboot_confirm_msg"]/div[4]/div/div/div[2]/div/div[2]/button/span[2]')
 Submit3.click()
 # click Yes at the popup to reboot
